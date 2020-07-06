46fe4452d2ff4f30850f0a6944d5107ccf7f5d1a
Gabriel Semaan <gabriel.semaan@cordian.com>
2020-07-06 18:38:28 +0200

    servig html

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const http = require('http');
+const fs = require('fs');
 
+const PORT = 5000;
 
-http.createServer((req, res) => {
-  res.writeHead(200, {"Content-Type": "text/plain"})
-  res.end(("Server running"))
-}).listen(process.env.PORT || 5000)
+fs.readFile('./index.html',  (err, html) => {
+
+  if (err) throw err;
+
+  http.createServer((req, res) => {
+    res.writeHead(200, {"Content-Type": "text/html"})
+    res.write(html);
+    res.end()
+  }).listen(process.env.PORT || PORT)
+});
