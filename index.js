6b62f1135d1743fedb2e26793cb08911f9935462
Gabriel Semaan <gabriel.semaan@cordian.com>
2020-07-07 16:34:12 +0200

    layout redesign

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
+const path = require('path');
 
-const PORT = 5000;
+const app = express();
+const port = process.env.PORT || 5000;
+const host = process.env.HOST_NAME || 'localhost';
 
-fs.readFile('./index.html',  (err, html) => {
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-  if (err) throw err;
+app.use(express.static(path.join(__dirname, 'public')));
 
-  http.createServer((req, res) => {
-    res.writeHead(200, {"Content-Type": "text/html"})
-    res.write(html);
-    res.end()
-  }).listen(process.env.PORT || PORT)
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`SERVER IS ALIVE >>> http://${host}:${port}`);
 });
