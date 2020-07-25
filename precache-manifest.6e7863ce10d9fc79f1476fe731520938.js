553289416c508e290a55e0bc71993cddf6916156
Gabriel Semaan <gabriel.semaan@cordian.com>
2020-07-25 17:04:53 +0300

    Updates

---
+++ precache-manifest.6e7863ce10d9fc79f1476fe731520938.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0b0ae408c68182a55fce350302cf03c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4839d6f243c9922db069",
+    "url": "/static/css/main.e70078cc.chunk.css"
+  },
+  {
+    "revision": "f71b8f12b0eba919dbd1",
+    "url": "/static/js/2.6d4d0610.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6d4d0610.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4839d6f243c9922db069",
+    "url": "/static/js/main.05e643d5.chunk.js"
+  },
+  {
+    "revision": "bba083550e2041d9d5fc",
+    "url": "/static/js/runtime-main.4279b0f6.js"
+  },
+  {
+    "revision": "952f14b484a7bb0efc92a710f843f927",
+    "url": "/static/media/backgroundImage.952f14b4.jpg"
+  },
+  {
+    "revision": "0fcad8c163c870995d2d8a9ed53ec151",
+    "url": "/static/media/headerText.0fcad8c1.svg"
+  },
+  {
+    "revision": "e61443ba856e711f84515a0b4e3da253",
+    "url": "/static/media/logo_black.e61443ba.svg"
+  }
+]);
