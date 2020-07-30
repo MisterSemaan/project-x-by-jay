bf0df92e41cee4e87e971cc085d6af380eb871d5
Gabriel Semaan <gabriel.semaan@cordian.com>
2020-07-30 16:29:07 +0300

    contact section

--- src/view/pages/Contact/components/socialMedia.js
+++ src/view/pages/Contact/components/socialMedia.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
-export const SocialMedia = ({ socialMediaTitle, socialMediaSrc, socialMediaAlt }) => {
+export const SocialMedia = ({ socialMediaTitle, socialMediaLink, socialMediaSrc, socialMediaAlt }) => {
   return (
     <div className="socialMediaWrapper">
       <h3>{socialMediaTitle}</h3>
-      <img src={socialMediaSrc} alt={socialMediaAlt} />
+      <a href={socialMediaLink}>
+        <img src={socialMediaSrc} alt={socialMediaAlt} />
+      </a>
     </div>
   )
 }
--- src/view/pages/Contact/contact.js
+++ src/view/pages/Contact/contact.js
@@ -2,15 +2,29 @@ import React from 'react';
 
 import { SocialMedia } from './components/socialMedia';
 
+import instagramIcon from '../../../assets/contact/instagramIcon.png';
+import facebookIcon from '../../../assets/contact/facebookIcon.png';
+
 import './contact.css';
 
 export const Contact = () => {
 
   return (
     <section id="contact" className="flex alignItemsCenter justifyCenter">
-      <SocialMedia socialMediaAlt="" socialMediaSrc="" socialMediaTitle=""/>
+      <SocialMedia
+        socialMediaAlt="Jamie Instagram"
+        socialMediaLink="https://www.instagram.com/project_x_by_ja_y_/"
+        socialMediaSrc={instagramIcon}
+        socialMediaTitle="Instagram: Project X by Jay"
+      />
+      <SocialMedia
+        socialMediaAlt="Jamie Facebook"
+        socialMediaLink="https://m.facebook.com/profile.php?id=100002461605262&ref=content_filter"
+        socialMediaSrc={facebookIcon}
+        socialMediaTitle="Facebook: Project X by Jay"
+      />
     </section>
   )
 }
 
-// See if I can use a contact form to prefill the body of an email client???
+// https://www.flaticon.com/search?search-type=icons&word=instagram
