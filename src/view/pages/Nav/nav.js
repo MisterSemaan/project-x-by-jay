44f10a2690a0829219500dc9781e3f3611783869
Gabriel Semaan <gabriel.semaan@cordian.com>
2020-08-01 17:55:07 +0300

    Fixed navigation

--- src/view/pages/Nav/nav.js
+++ src/view/pages/Nav/nav.js
@@ -8,24 +8,24 @@ export const Nav = () => {
     <aside>
       <nav id="menu-container">
         <ul className="flex flexColumn alignItemsCenter justifyAround">
-          <li className="home flex alignItemsCenter justifyCenter flexOne menu-item">
-            <Link className="linkItem" to='/'>Home</Link>
-          </li>
-          <li className="stream flex alignItemsCenter justifyCenter flexOne menu-item">
-            <Link className="linkItem" to='/stream'>Stream</Link>
-          </li>
-          <li className="about flex alignItemsCenter justifyCenter flexOne menu-item">
-            <Link className="linkItem" to='/about'>About</Link>
-          </li>
-          <li className="gallery flex alignItemsCenter justifyCenter flexOne menu-item">
-            <Link className="linkItem" to='/gallery'>Gallery</Link>
-          </li>
-          <li className="partner flex alignItemsCenter justifyCenter flexOne menu-item">
-            <Link className="linkItem" to='/partner'>Partner</Link>
-          </li>
-          <li className="contact flex alignItemsCenter justifyCenter flexOne menu-item">
-            <Link className="linkItem" to='/contact'>Contact</Link>
-          </li>
+          <Link className="home flex alignItemsCenter justifyCenter flexOne menu-item" to='/'>
+            <li className="linkItem ">Home</li>
+          </Link>
+            <Link className="stream flex alignItemsCenter justifyCenter flexOne menu-item" to='/stream'>
+              <li className="linkItem ">Stream</li>
+            </Link>
+            <Link className="about flex alignItemsCenter justifyCenter flexOne menu-item" to='/about'>
+              <li className="linkItem ">About</li>
+            </Link>
+          <Link className="gallery flex alignItemsCenter justifyCenter flexOne menu-item" to='/gallery'>
+            <li className="linkItem ">Gallery</li>
+          </Link>
+          <Link className="partner flex alignItemsCenter justifyCenter flexOne menu-item" to='/partner'>
+            <li className="linkItem ">Partner</li>
+          </Link>
+          <Link className="contact flex alignItemsCenter justifyCenter flexOne menu-item" to='/contact'>
+            <li className="linkItem ">Contact</li>
+          </Link>
         </ul>
       </nav>
     </aside>
