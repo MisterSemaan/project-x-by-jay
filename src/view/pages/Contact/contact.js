a4d93e31771d7ca2d39132a00db9fad24fbc77cb
Gabriel Semaan <gabriel.semaan@cordian.com>
2020-08-01 22:41:17 +0300

    new social medias

--- src/view/pages/Contact/contact.js
+++ src/view/pages/Contact/contact.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 import { SocialMedia } from './components/socialMedia';
 
-import instagramIcon from '../../../assets/contact/instagramIcon.png';
-import facebookIcon from '../../../assets/contact/facebookIcon.png';
+import instagramIcon from '../../../assets/contact/instagram.svg';
+import facebookIcon from '../../../assets/contact/facebook.svg';
+import youtubeIcon from '../../../assets/contact/youtube.svg';
 
 import './contact.css';
 
@@ -12,16 +13,22 @@ export const Contact = () => {
   return (
     <section id="contact" className="flex alignItemsCenter justifyCenter">
       <SocialMedia
-        socialMediaAlt="Jamie Instagram"
+        socialMediaAlt="Project X Instagram"
         socialMediaLink="https://www.instagram.com/project_x_by_ja_y_/"
         socialMediaSrc={instagramIcon}
         socialMediaTitle="Instagram: Project X by Jay"
       />
       <SocialMedia
-        socialMediaAlt="Jamie Facebook"
-        socialMediaLink="https://m.facebook.com/profile.php?id=100002461605262&ref=content_filter"
+        socialMediaAlt="Project X Facebook"
+        socialMediaLink="https://www.facebook.com/ProjectXbyJay"
         socialMediaSrc={facebookIcon}
         socialMediaTitle="Facebook: Project X by Jay"
+      />
+      <SocialMedia
+        socialMediaAlt="Project X Youtube"
+        socialMediaLink="https://www.youtube.com/channel/UC6XTYyA0VW6IQVXCAybVYQw"
+        socialMediaSrc={youtubeIcon}
+        socialMediaTitle="Youtube: Project X by Jay"
       />
     </section>
   )
